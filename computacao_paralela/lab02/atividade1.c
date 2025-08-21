54db2e505f1f945f8a597af7a6cf546a48691147
Thais Canguçu <160797281+Thaiscangucu@users.noreply.github.com>
2025-08-21 23:54:33 +0000

    criado o arquivo da atividade 1 do lab02

---
+++ computacao_paralela/lab02/atividade1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+
+//paralelizar este código
+//comando pra compilar código: gcc lab02.c -o lab02 -lm
+
+
+#define VECTOR_SIZE 200000000
+
+// Função que simula uma carga de trabalho pesada
+void heavy_work(double *vector, int start, int end) {
+    for (int i = start; i < end; ++i) {
+        vector[i] = sin(vector[i]) * cos(vector[i]) + sqrt(vector[i]);
+    }
+}
+
+int main() {
+    double *vector = (double *) malloc(VECTOR_SIZE * sizeof(double));
+    if (vector == NULL) {
+        fprintf(stderr, "Erro ao alocar memória\n");
+        return 1;
+    }
+
+    for (int i = 0; i < VECTOR_SIZE; i++) {
+        vector[i] = (double) i;
+    }
+
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+
+    heavy_work(vector, 0, VECTOR_SIZE);
+
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    double time_spent = (end.tv_sec - start.tv_sec) + 
+                        (end.tv_nsec - start.tv_nsec) / 1e9;
+
+    printf("Versão sequencial executou em %f segundos\n", time_spent);
+
+    free(vector);
+    return 0;
+}
